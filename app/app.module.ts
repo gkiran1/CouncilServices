5749405f0c258b1d81fa7783b8f5a9c066a580e7
Poojitha Thunuguntla <tpoojitha@prokarma.com>
2017-04-17 17:36:02 +0530

    imported from Angular fire 2 from firebase.

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,15 +5,25 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
+import * as firebase from 'firebase';
 import { AngularFireModule } from 'angularfire2';
+// import { FirebaseConfig } from './../environments/firebase/firebase-config';
 
 /* Feature Modules */
 import { ProductModule } from './products/product.module';
 
+export const FirebaseConfig = {
+    apiKey: "AIzaSyDauCRlokcRecT283636ODNP2z4qZe2oPA",
+    authDomain: "lds-councils-8f56b.firebaseapp.com",
+    databaseURL: "https://lds-councils-8f56b.firebaseio.com",
+    storageBucket: "lds-councils-8f56b.appspot.com",
+    messagingSenderId: "619253720821"
+}
+
 @NgModule({
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(FirebaseConfig),
     HttpModule,
     RouterModule.forRoot([
       { path: 'welcome', component: WelcomeComponent },
