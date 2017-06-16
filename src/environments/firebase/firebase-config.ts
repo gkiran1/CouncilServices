317dac938c9f6388f702ac68ff775f514a3aa3a4
tbharati <tbharati@prokarma.com>
2017-06-16 17:34:43 +0530

    Config update

--- src/environments/firebase/firebase-config.ts
+++ src/environments/firebase/firebase-config.ts
@@ -1,8 +1,15 @@
 export const FirebaseConfig = {
-    apiKey: "AIzaSyDXnywKsXMwYNmHtLjzQZxa52jhWrUAcD0",
-    authDomain: "councils-45092.firebaseapp.com",
-    databaseURL: "https://councils-45092.firebaseio.com",
-    projectId: "councils-45092",
-    storageBucket: "councils-45092.appspot.com",
-    messagingSenderId: "310008879834"
+    // apiKey: "AIzaSyDXnywKsXMwYNmHtLjzQZxa52jhWrUAcD0",
+    // authDomain: "councils-45092.firebaseapp.com",
+    // databaseURL: "https://councils-45092.firebaseio.com",
+    // projectId: "councils-45092",
+    // storageBucket: "councils-45092.appspot.com",
+    // messagingSenderId: "310008879834"
+
+    apiKey: "AIzaSyA7vboizvDrRORHvkT01Z8vKCOuH8In4UA",
+    authDomain: "councilsdb.firebaseapp.com",
+    databaseURL: "https://councilsdb.firebaseio.com",
+    projectId: "councilsdb",
+    storageBucket: "councilsdb.appspot.com",
+    messagingSenderId: "92555477583"
 }
