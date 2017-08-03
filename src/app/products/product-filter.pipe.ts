1905e7aa8d6e0829e1e60eb53618f0832ce4abdc
KambagiriSwamyB <bkambagiri@prokarma.com>
2017-08-03 12:09:36 +0530

    update to filter

--- src/app/products/product-filter.pipe.ts
+++ src/app/products/product-filter.pipe.ts
@@ -9,7 +9,7 @@ export class ProductFilterPipe implements PipeTransform {
     transform(value: IProduct[], filterBy: string): IProduct[] {
         filterBy = filterBy ? filterBy.toLocaleLowerCase() : null;
         return filterBy ? value.filter((product: IProduct) =>
-            ((product.UnitNum.toString().toLocaleLowerCase().indexOf(filterBy) !== -1) ||
-                (product.UnitName.toString().toLocaleLowerCase().indexOf(filterBy) !== -1))) : value;
+            ((product.UnitNum && product.UnitNum.toString().toLocaleLowerCase().indexOf(filterBy) !== -1) ||
+                (product.UnitName && product.UnitName.toString().toLocaleLowerCase().indexOf(filterBy) !== -1))) : value;
     }
 }
